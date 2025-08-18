d4b6765545711551d9bd4716ad2b1c57a17d78bd
igorgalind0 <igorgalindo950@gmail.com>
2025-08-18 16:12:37 -0300

    adicionando link para os projetos e dashboards e carrossel dos parceiros

--- script.js
+++ script.js
@@ -16,3 +16,39 @@ const ulMenu = document.querySelector("[data-menu='menu-list'");
 menuMobile.addEventListener('click', () => {
     ulMenu.classList.toggle('ativo');
 });
+
+window.onload = () => {
+    const list = document.getElementById('image-list');
+    const items = lista.querySelectorAll('li');
+
+    items.forEach(item => {
+        const clone = item.cloneNode(true);
+        list.appendChild(clone);
+    });
+}
+
+const linksInternos = document.querySelectorAll('a[href^="#"]');
+
+function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    if (section) {
+      const topo = section.offsetTop - 100;
+      window.scrollTo({
+          top: topo,
+          behavior: 'smooth',
+      });
+  }
+
+    // forma alternativa
+    // const topo = section.offsetTop;
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: 'smooth',
+    // });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+});
